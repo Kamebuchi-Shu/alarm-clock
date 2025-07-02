a21bb419b42f5b45ffffbf7c3390f40492f9d754
TK386 <deds1659185348@gmail.com>
2025-07-02 15:14:10 +0900

    main_activity,check_activityの変更統合

--- Alarm-Clock-master/app/src/main/java/com/example/ogatafutoshikawa/alarm_clock/Check_Activity.java
+++ Alarm-Clock-master/app/src/main/java/com/example/ogatafutoshikawa/alarm_clock/Check_Activity.java
@@ -5,8 +5,10 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,28 +17,41 @@ import java.util.Calendar;
 
 public class Check_Activity extends AppCompatActivity {
 
+    private static final String TAG = "Check_Activity"; // ログ用タグ
+
     /**
      * City_Activityの画面構成をするメソッド
      * @param savedInstanceState
      */
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cheack);
 
         Intent intent = getIntent();
+
+        // 時間と分を取得
+        int get_hour = intent.getIntExtra(Main_Activity.HOUR_DATA, 0);
+        int get_min = intent.getIntExtra(Main_Activity.MIN_DATA, 0);
+        //String get_prefecture = intent.getStringExtra(Main_Activity.PREFECTURE_DATA);
+        //String get_city = intent.getStringExtra(Main_Activity.CITY_DATA);
+
+        // 一意なリクエストコード生成 (時間と分を組み合わせ)
+        int requestCode = get_hour * 100 + get_min;
+
+        // アラーム用のIntent
         Intent bootIntent = new Intent(Check_Activity.this, Alarm_Receiver.class);
+        bootIntent.putExtra("notificationId", requestCode);
 
-        final PendingIntent alarmIntent = PendingIntent.getBroadcast(Check_Activity.this, 0, bootIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        // PendingIntentの作成 (FLAG_UPDATE_CURRENTを使用)
+        final PendingIntent alarmIntent = PendingIntent.getBroadcast(
+                Check_Activity.this,
+                requestCode,  // 一意なリクエストコード
+                bootIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
         final AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        int notificationId = 0;
-        bootIntent.putExtra("notificationId", notificationId);
-
-        int get_hour = intent.getIntExtra(Main_Activity.HOUR_DATA,0);
-        int get_min = intent.getIntExtra(Main_Activity.MIN_DATA,0);
-        String get_prefecture = intent.getStringExtra(Main_Activity.PREFECTURE_DATA);
-        String get_city = intent.getStringExtra(Main_Activity.CITY_DATA);
 
         Button btnReset = this.findViewById(R.id.reset);
         btnReset.setOnClickListener(new View.OnClickListener() {
@@ -47,22 +62,52 @@ public class Check_Activity extends AppCompatActivity {
             }
         });
 
-        changePre(get_prefecture);
-        changeCity(get_city);
+        //changePre(get_prefecture);
+        //changeCity(get_city);
         changeTime2(get_hour, get_min);
 
+        // アラーム時間の設定
         Calendar startTime = Calendar.getInstance();
         startTime.set(Calendar.HOUR_OF_DAY, get_hour);
         startTime.set(Calendar.MINUTE, get_min);
         startTime.set(Calendar.SECOND, 0);
+        startTime.set(Calendar.MILLISECOND, 0);
         long alarmStartTime = startTime.getTimeInMillis();
 
-        alarm.set(
-                //デバイスがスリープ状態の場合に alarm が動作した場合、alarm はデバイスを起こす
-                AlarmManager.RTC_WAKEUP,
-                alarmStartTime,
-                alarmIntent         // PendingIntent
-        );
+        // 過去の時間に設定された場合は翌日にする
+        if (alarmStartTime <= System.currentTimeMillis()) {
+            alarmStartTime += 24 * 60 * 60 * 1000; // 1日追加
+            Log.d(TAG, "過去の時刻設定を検出、翌日に設定: " + get_hour + ":" + get_min);
+        }
+
+        // Androidバージョンに応じたアラーム設定
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android 6.0+ (Dozeモード対策)
+            alarm.setExactAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP,
+                    alarmStartTime,
+                    alarmIntent
+            );
+            Log.d(TAG, "setExactAndAllowWhileIdleを使用");
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Android 4.4+
+            alarm.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    alarmStartTime,
+                    alarmIntent
+            );
+            Log.d(TAG, "setExactを使用");
+        } else {
+            // 旧バージョン
+            alarm.set(
+                    AlarmManager.RTC_WAKEUP,
+                    alarmStartTime,
+                    alarmIntent
+            );
+            Log.d(TAG, "setを使用");
+        }
+
+        Log.d(TAG, "アラーム設定: " + get_hour + ":" + get_min + " (リクエストコード: " + requestCode + ")");
     }
 
     /**
@@ -72,14 +117,8 @@ public class Check_Activity extends AppCompatActivity {
      */
     @SuppressLint("SetTextI18n")
     public void changeTime2(int h, int m){
-        String hspace = "";
-        String mspace = "";
-        if(h < 10){
-            hspace = "0";
-        }
-        if(m < 10){
-            mspace = "0";
-        }
+        String hspace = h < 10 ? "0" : "";
+        String mspace = m < 10 ? "0" : "";
         TextView tv = findViewById(R.id.time2);
         tv.setText(hspace + h + ":" + mspace + m);
     }
@@ -88,17 +127,17 @@ public class Check_Activity extends AppCompatActivity {
      *
      * @param str
      */
-    public void changePre(String str) {
+    /*public void changePre(String str) {
         TextView tv = findViewById(R.id.prefecture2);
         tv.setText(str);
-    }
+    }*/
 
     /**
      *
      * @param str
      */
-    public void changeCity(String str) {
+    /*public void changeCity(String str) {
         TextView tv = findViewById(R.id.city2);
         tv.setText(str);
-    }
+    }*/
 }
--- Alarm-Clock-master/app/src/main/java/com/example/ogatafutoshikawa/alarm_clock/Main_Activity.java
+++ Alarm-Clock-master/app/src/main/java/com/example/ogatafutoshikawa/alarm_clock/Main_Activity.java
@@ -9,217 +9,198 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * メインのクラス。AppCompatActivityクラスを継承。View.OnClickListenerをインポートしている。
- * @version 1.0
- * @author Ogata
- */
-public class Main_Activity extends AppCompatActivity
-                            implements View.OnClickListener{
+public class Main_Activity extends AppCompatActivity implements View.OnClickListener {
 
-    //データ受け渡しの際に使うkey
-    /**Alarm_Activityクラスへの受け渡しの認証キー*/
+    // データ受け渡しの際に使うkey
     public static final int REQUEST_TIME = 0;
+    public static final int REQUEST_PREFECTURE = 1;
+    public static final int REQUEST_CITY = 2;
+    public static final int REQUEST_AUDIO = 3;
 
-    /**Prefecture_Activityクラスへの受け渡しの認証キー*/
-    public static final int REQUEST_PREFECTURE= 1;
-
-    /**City_Activityクラスへの受け渡しの認証キー*/
-    public static final int REQUEST_CITY= 2;
-
-    /**City_Activityクラスにどの県を選択したかのデータを渡す際に使う認証キー*/
-    public static final String PRE_NUM= "num";
-
-    /**Check_Activityクラスに"時間"のデータを渡す際に使う認証キー*/
-    public static final String HOUR_DATA= "0";
-
-    /**Check_Activityクラスに"分"のデータを渡す際に使う認証キー*/
-    public static final String MIN_DATA= "1";
-
-    /**Check_Activityクラスに"県"のデータを渡す際に使う認証キー*/
-    public static final String PREFECTURE_DATA= "prefecture";
-
-    /**Check_Activityクラスに"市"のデータを渡す際に使う認証キー*/
+    public static final String PRE_NUM = "num";
+    public static final String HOUR_DATA = "0";
+    public static final String MIN_DATA = "1";
+    public static final String PREFECTURE_DATA = "prefecture";
     public static final String CITY_DATA = "city";
 
-    //変数宣言
-    /**Prefecture_Activityクラスから受け取ったデータを格納するフィールド*/
-    String pre_str = null;
+    // 変数宣言
+    private String pre_str = null;
+    private String city_str = null;
+    private String save_pre = null;
+    private String pre_key = null;
+    private String city_key = null;
+    private String place_key = null;
+    private int pre_num = 0;
+    private int hour = 100;
+    private int min = 100;
+    private String selectedAudioName = "デフォルト音声"; // 選択された音声名を保持
 
-    /**City_Activityクラスから受け取ったデータを格納するフィールド*/
-    String city_str = null;
-
-    /**pre_strのデータを保存するためのフィールド（変更した時Cityのところを空白にするため）*/
-    String save_pre = null;
-
-    /**県の番号。01~47の2桁*/
-    String pre_key = null;
-
-    /**市の番号。県によってバラバラの４桁*/
-    String city_key = null;
-
-    /**県の番号と市の番号を足したもの*/
-    String place_key = null;
-
-    /**県の番号。市の番号を検索する際に単純化させるため*/
-    int pre_num = 0;
-
-    /**Alarm_Activityクラスから受け取った"時間"のデータを格納するフィールド*/
-    int hour = 100;
-
-    /**Alarm_Activityクラスから受け取った"分"のデータを格納するフィールド*/
-    int min = 100;
-
-
-    /**
-     * Main_Activityの画面を構成するメソッド
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set);
 
-        //各ボタンのオブジェクト化
-        Button btnTime = this.findViewById(R.id.time);
-        Button btnPrefecture = this.findViewById(R.id.prefecture);
-        Button btnCity = this.findViewById(R.id.city);
-        Button btnCheack = this.findViewById(R.id.cheack);
+        // 各ボタンのオブジェクト化
+        Button btnTime = findViewById(R.id.time);
+        Button btnPrefecture = findViewById(R.id.prefecture);
+        Button btnCity = findViewById(R.id.city);
+        Button btnCheack = findViewById(R.id.cheack);
+        Button btnAudio = findViewById(R.id.audio_select);
 
-        //各ボタンのクリック判定
+        // 各ボタンのクリックリスナー設定
         btnTime.setOnClickListener(this);
         btnPrefecture.setOnClickListener(this);
         btnCity.setOnClickListener(this);
         btnCheack.setOnClickListener(this);
+        btnAudio.setOnClickListener(this);
+
+        // 保存された音声名を読み込んで表示
+        loadSelectedAudioName();
     }
 
+    // 保存された音声名を読み込む
+    private void loadSelectedAudioName() {
+        SharedPreferences prefs = getSharedPreferences("alarm_prefs", MODE_PRIVATE);
+        String savedName = prefs.getString("alarm_sound_name", null);
 
-    /**
-     * 時間のブロックを更新するメソッド
-     * @param hour
-     * @param min
-     */
-    //時間のブロックに指定した時間を表示
+        if (savedName != null) {
+            selectedAudioName = savedName;
+        }
+
+        // 音声名を表示
+        changeAudioName(selectedAudioName);
+    }
+
     @SuppressLint("SetTextI18n")
     public void changeTime(int hour, int min) {
         TextView tv = findViewById(R.id.time);
-        String hspace = "";
-        String mspace = "";
-        if(hour < 10){
-            hspace = "0";
-        }
-        if(min < 10){
-            mspace = "0";
-        }
-        tv.setText(hspace + hour + ":"+ mspace + min);
+        String hspace = hour < 10 ? "0" : "";
+        String mspace = min < 10 ? "0" : "";
+        tv.setText(hspace + hour + ":" + mspace + min);
     }
 
-    /**
-     * 県のブロックを更新するメソッド
-     * @param str
-     */
-    //県のブロックに指定した県を表示
-    public void changePrefecture(String str) {
+    /*public void changePrefecture(String str) {
         TextView tv = findViewById(R.id.prefecture);
         tv.setText(str);
     }
 
-    /**
-     * 市のブロックを更新するメソッド
-     * @param str
-     */
-    //市のブロックに指定した県を表示
     public void changeCity(String str) {
         TextView tv = findViewById(R.id.city);
         tv.setText(str);
+    }*/
+
+    // 選択された音声名を表示
+    private void changeAudioName(String name) {
+        TextView tv = findViewById(R.id.audio_name);
+        if (name != null && !name.isEmpty()) {
+            tv.setText(name);
+        } else {
+            tv.setText("デフォルト音声");
+        }
     }
 
-    /**
-     * クリックイベントのメソッド
-     * @param v
-     */
-    //各クリックのイベント
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if(id == R.id.time) {
-            Intent intent1 = new Intent(Main_Activity.this, Alarm_Activity.class);
-            startActivityForResult(intent1,REQUEST_TIME);
-        } else if (id == R.id.prefecture) {
-            Intent intent2 = new Intent(Main_Activity.this, Prefecture_Activity.class);
-            startActivityForResult(intent2,REQUEST_PREFECTURE);
+        if (id == R.id.time) {
+            Intent intent = new Intent(this, Alarm_Activity.class);
+            startActivityForResult(intent, REQUEST_TIME);
+        } /*else if (id == R.id.prefecture) {
+            Intent intent = new Intent(this, Prefecture_Activity.class);
+            startActivityForResult(intent, REQUEST_PREFECTURE);
         } else if (id == R.id.city) {
-            if(pre_num != 0) {
-                Intent intent3 = new Intent(Main_Activity.this, City_Activity.class);
-                intent3.putExtra(PRE_NUM,pre_num);
-                startActivityForResult(intent3,REQUEST_CITY);
+            if (pre_num != 0) {
+                Intent intent = new Intent(this, City_Activity.class);
+                intent.putExtra(PRE_NUM, pre_num);
+                startActivityForResult(intent, REQUEST_CITY);
+            } else {
+                Toast.makeText(this, "まず都道府県を選択してください", Toast.LENGTH_SHORT).show();
             }
-        } else if (id ==R. id.cheack) {
-            if(hour < 100 && min < 100 && pre_str != null && city_str != null) {
+        } */else if (id == R.id.audio_select) {
+            Intent intent = new Intent(this, AudioSelectActivity.class);
+            startActivityForResult(intent, REQUEST_AUDIO);
+        } else if (id == R.id.cheack) {
+            if (hour < 100 && min < 100 && pre_str != null && city_str != null) {
                 place_key = pre_key + city_key;
-                Intent intent4 = new Intent(Main_Activity.this, Check_Activity.class);
-                intent4.putExtra(HOUR_DATA, hour);
-                intent4.putExtra(MIN_DATA, min);
-                intent4.putExtra(PREFECTURE_DATA, pre_str);
-                intent4.putExtra(CITY_DATA, city_str);
 
-                SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = data.edit();
-                editor.putString("Save", place_key);
-                editor.apply();
+                Intent intent = new Intent(this, Check_Activity.class);
+                intent.putExtra(HOUR_DATA, hour);
+                intent.putExtra(MIN_DATA, min);
+                /*intent.putExtra(PREFECTURE_DATA, pre_str);
+                intent.putExtra(CITY_DATA, city_str);*/
+                intent.putExtra("audio_name", selectedAudioName); // 音声名を渡す
 
-                startActivity(intent4);
+                // 音声名を保存
+                SharedPreferences prefs = getSharedPreferences("alarm_prefs", MODE_PRIVATE);
+                prefs.edit()
+                        .putString("alarm_sound_name", selectedAudioName)
+                        .apply();
+
+                startActivity(intent);
+            } else {
+                String message = "設定されていない項目: ";
+                if (hour >= 100 || min >= 100) message += "時間 ";
+                /*if (pre_str == null) message += "都道府県 ";
+                if (city_str == null) message += "市区町村";*/
+
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    /**
-     * 各Activityからのデータを受け取るメソッド
-     * @param requestCode
-     * @param resultCode
-     * @param intent
-     */
-    //他のActivityからのデータの受け取り
     @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent intent){
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        Bundle bundle;
 
-        switch(requestCode){
-            case (REQUEST_PREFECTURE):
-                if(resultCode == RESULT_OK){
-                    bundle = intent.getExtras();
-                    assert bundle != null;
-                    pre_str = bundle.getString("prefecture");
-                    pre_key = bundle.getString("pre_key");
-                    pre_num = bundle.getInt("pre_num");
-                    changePrefecture(pre_str);
-                    if(save_pre != pre_str){
-                        changeCity("---");
-                    }
-                    save_pre = pre_str;
+        // 結果が正常でない場合は処理をスキップ
+        if (resultCode != RESULT_OK) return;
+        if (intent == null) return;
+
+        // 音声選択結果は必ずEXTRAがあるのでbundle不要
+        if (requestCode == REQUEST_AUDIO) {
+            // 音声選択結果を処理
+            if (intent.hasExtra(AudioSelectActivity.EXTRA_AUDIO_NAME)) {
+                selectedAudioName = intent.getStringExtra(AudioSelectActivity.EXTRA_AUDIO_NAME);
+                changeAudioName(selectedAudioName);
+
+                // 音声名を保存
+                SharedPreferences prefs = getSharedPreferences("alarm_prefs", MODE_PRIVATE);
+                prefs.edit()
+                        .putString("alarm_sound_name", selectedAudioName)
+                        .apply();
+            }
+            return; // 他の処理をスキップ
+        }
+
+        // その他のリクエストはbundleを使用
+        Bundle bundle = intent.getExtras();
+        if (bundle == null) return;
+
+        switch (requestCode) {
+            /*case REQUEST_PREFECTURE:
+                pre_str = bundle.getString("prefecture");
+                pre_key = bundle.getString("pre_key");
+                pre_num = bundle.getInt("pre_num");
+                changePrefecture(pre_str);
+                if (save_pre == null || !save_pre.equals(pre_str)) {
+                    city_str = null; // 都道府県が変わったら市区町村をリセット
+                    changeCity("---");
                 }
+                save_pre = pre_str;
                 break;
 
-            case (REQUEST_CITY):
-                if(resultCode == RESULT_OK){
-                    bundle = intent.getExtras();
-                    assert bundle != null;
-                    city_str= bundle.getString("city");
-                    city_key = bundle.getString("city_key");
-                    changeCity(city_str);
-                }
-                break;
+            case REQUEST_CITY:
+                city_str = bundle.getString("city");
+                city_key = bundle.getString("city_key");
+                changeCity(city_str);
+                break;*/
 
-            case(REQUEST_TIME):
-                if(resultCode == RESULT_OK) {
-                    bundle = intent.getExtras();
-                    assert bundle != null;
-                    hour = bundle.getInt("hour");
-                    min = bundle.getInt("min");
-                    changeTime(hour,min);
-                }
+            case REQUEST_TIME:
+                hour = bundle.getInt("hour");
+                min = bundle.getInt("min");
+                changeTime(hour, min);
                 break;
         }
     }
