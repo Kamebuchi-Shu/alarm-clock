35d781373871e64862e1ab97db5edc54129e7062
haru <ikedaharu.work.1004@gmail.com>
2025-07-07 21:34:25 +0900

    デバック画面の追加

--- Alarm-Clock-master/app/src/main/java/com/example/ogatafutoshikawa/alarm_clock/DebugActivity.java
+++ Alarm-Clock-master/app/src/main/java/com/example/ogatafutoshikawa/alarm_clock/DebugActivity.java
@@ -8,9 +8,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
